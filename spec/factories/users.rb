51752d3e7dab3a988c2d8b73ae0eb80ed645da74
naa4263 <naa42638@gmail.com>
2026-03-14 22:09:48 +0900

    ユーザー管理修正

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -4,5 +4,10 @@ FactoryBot.define do
     email                 { 'test@example' }
     password              { '000000' }
     password_confirmation { password }
+    last_name             { 'テスト' }
+    first_name            { 'テスト' }
+    last_name_kana        { 'テスト' }
+    first_name_kana       { 'テスト' }
+    birthday              { '2000-01-01' }
   end
 end
