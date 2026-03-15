678506854fc125edcdacf5b0a96234254e4b3d0d
naa4263 <naa42638@gmail.com>
2026-03-15 16:55:01 +0900

    ユーザー管理修正

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,16 +2,24 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   describe 'ユーザー新規登録' do
-    it 'nicknameが空では登録できない' do
-      user = FactoryBot.build(:user)
-      user.valid?
-      expect(user.errors.full_messages).to include("Nickname can't be blank")
+    context '正常形' do
+      it '正しく項目を入力したら登録できる' do
+        user = FactoryBot.build(:user)
+        expect(user).to be_valid
+      end
     end
-    it 'emailが空では登録できない' do
-      user = FactoryBot.build(:user)
 
-      user.valid?
-      expect(user.errors.full_messages).to include("Email can't be blank")
+    context '異常形' do
+      it 'nicknameが空では登録できない' do
+        user = FactoryBot.build(:user, nickname: '')
+        user.valid?
+        expect(user.errors.full_messages).to include("Nickname can't be blank")
+      end
+      it 'emailが空では登録できない' do
+        user = FactoryBot.build(:user, nickname: '')
+        user.valid?
+        expect(user.errors.full_messages).to include("Email can't be blank")
+      end
     end
   end
 end
