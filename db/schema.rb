348816735f9b33e676b4c7b82a2345be700a12f3
naa4263 <naa42638@gmail.com>
2026-03-22 17:35:59 +0900

    一覧表示

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2026_03_17_111612) do
+ActiveRecord::Schema[7.1].define(version: 2026_03_17_104426) do
   create_table "active_storage_attachments", charset: "utf8mb3", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -37,22 +37,6 @@ ActiveRecord::Schema[7.1].define(version: 2026_03_17_111612) do
     t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
-  end
-
-  create_table "articles", charset: "utf8mb3", force: :cascade do |t|
-    t.string "product", null: false
-    t.text "explanation", null: false
-    t.integer "category_id", null: false
-    t.integer "situation_id", null: false
-    t.integer "delivery_fee_id", null: false
-    t.integer "prefecture_id", null: false
-    t.integer "days_id", null: false
-    t.integer "price", null: false
-    t.bigint "user_id", null: false
-    t.integer "genre_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_articles_on_user_id"
   end
 
   create_table "items", charset: "utf8mb3", force: :cascade do |t|
@@ -90,6 +74,5 @@ ActiveRecord::Schema[7.1].define(version: 2026_03_17_111612) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "articles", "users"
   add_foreign_key "items", "users"
 end
