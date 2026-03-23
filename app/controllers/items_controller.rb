a871d6c89cf01c911052fd2a9c7fb69f42663f34
naa4263 <naa42638@gmail.com>
2026-03-24 08:40:32 +0900

    情報編集

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
-  before_action :move_to_index, only: [:edit, :update]
+  before_action :authenticate_user!, except: [:index, :show]
   def index
     @items = Item.order('created_at DESC')
   end
